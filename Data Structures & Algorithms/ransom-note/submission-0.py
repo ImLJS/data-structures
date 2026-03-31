51dbe7b64b0805f0738c9eba35d6ccfa29f29d80
Leone Jacob Sunil <109210868+ImLJS@users.noreply.github.com>
2026-03-31 08:44:21 +0530

    Add: ransom-note - submission-0

---
+++ Data Structures & Algorithms/ransom-note/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for ch in ransomNote:
+            if ch in magazine:
+                magazine = magazine.replace(ch, '', 1)
+            else:
+                return False
+        return True
