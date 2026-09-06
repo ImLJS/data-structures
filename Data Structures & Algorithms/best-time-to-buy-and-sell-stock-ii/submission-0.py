40c9dea245daebc9cd8acb039dc907d698942e6e
Leo <109210868+heyImLeo@users.noreply.github.com>
2026-09-06 07:26:00 +0530

    Update: best-time-to-buy-and-sell-stock-ii - submission-0

--- Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.py
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.py
@@ -1,9 +1,9 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        cost = 0
+        profit = 0
 
         for i in range(1, len(prices)):
             if prices[i] > prices[i-1]:
-                cost += (prices[i] - prices[i-1])
+                profit += (prices[i] - prices[i-1])
         
-        return cost
+        return profit
